f190d379f82b3a13890fb319c1c4fa63e6867433
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-09-14 14:52:39 +0430

    using fsPromise instead of fs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 fs.readFile(
@@ -12,23 +13,55 @@ fs.readFile(
   }
 );
 
-fs.writeFile(
-  path.join(__dirname, 'files', 'texts', 'test.txt'),
-  'Nice to meet you.',
-  (err) => {
-    if (err) throw err;
-    console.log('write completed');
-    fs.appendFile(
+// fs.writeFile(
+//   path.join(__dirname, 'files', 'texts', 'test.txt'),
+//   'Nice to meet you.',
+//   (err) => {
+//     if (err) throw err;
+//     console.log('write completed');
+//     fs.appendFile(
+//       path.join(__dirname, 'files', 'texts', 'test.txt'),
+//       '\n \n yes it is.',
+//       (err) => {
+//         if (err) throw err;
+//         console.log('append completed');
+//       }
+//     );
+//   }
+// );
+
+// ! USING FS-PROMISE INSTEAD OF FS
+fsPromises
+  .writeFile(
+    path.join(__dirname, 'files', 'texts', 'test.txt'),
+    'hello world',
+    (err) => {
+      if (err) throw err;
+      console.log('write completed');
+    }
+  )
+  .then(
+    fsPromises.appendFile(
       path.join(__dirname, 'files', 'texts', 'test.txt'),
-      '\n \n yes it is.',
+      '\n \n from new world',
       (err) => {
         if (err) throw err;
         console.log('append completed');
       }
-    );
-  }
-);
+    )
+  )
+  .then(
+    fsPromises.rename(
+      path.join(__dirname, 'files', 'texts', 'test.txt'),
+      path.join(__dirname, 'files', 'texts', 'example.txt')
+    ),
+    (err) => {
+      if (err) throw err;
+      console.log('rename completed');
+    }
+  );
 
+// ERROR CALLBACK
 process.on('uncaughtException', (err) => {
   console.log('uncaught error finded', err);
   process.exit();
